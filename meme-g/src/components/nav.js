22fbd8a21473deebef2d329ad6a8452a6d9c1d2c
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-03-25 16:10:04 +0000

    push update

--- meme-g/src/components/nav.js
+++ meme-g/src/components/nav.js
@@ -4,7 +4,7 @@ export default class Nav extends Component{
     render() {
         return (
             <nav className='nav'>
-
+                <img id='nav-logo' src='images/trollface.jpg'></img>
             </nav>
         )
     }
