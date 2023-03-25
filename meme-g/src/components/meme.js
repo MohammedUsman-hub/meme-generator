3283643f2abee969d9c7b04e0c1c72477d6a2c23
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-03-25 16:42:45 +0000

    setting up meme.js

--- meme-g/src/components/meme.js
+++ meme-g/src/components/meme.js
@@ -1,2 +1,12 @@
 import React, { Component } from 'react'
 import "../App.css"
+
+export default class Meme extends Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
