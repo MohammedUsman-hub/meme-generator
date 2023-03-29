d45360453f4aee7484f06d033d7feaef8122cc9d
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-03-29 23:33:35 +0100

    break

--- meme-g/src/components/meme.js
+++ meme-g/src/components/meme.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 import "../App.css"
 
+import memesData from '../memesData';
+
+
+    function imageClick(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+    }
+
 export default class Meme extends Component{
     render(){
+       
         return(
+            
             <main>
                 <div className='form'>
 
                     <input placeholder='Top-text' className='form-input' type="text"/>
                     <input placeholder='Bottom-text' className='form-input' type="text"/>
-                    <button className='form-button'>Generate a new meme image</button>
+                    <button onClick={imageClick} className='form-button'>Generate a new meme image</button>
                 </div>
             </main>
         );
