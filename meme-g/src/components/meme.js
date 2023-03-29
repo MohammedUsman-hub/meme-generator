c888bf5b9ac174424fddb62de3c8ca2f65116e97
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-03-29 23:09:44 +0100

    adding memesData.js

--- meme-g/src/components/meme.js
+++ meme-g/src/components/meme.js
@@ -5,12 +5,12 @@ export default class Meme extends Component{
     render(){
         return(
             <main>
-                <form className='form'>
+                <div className='form'>
 
                     <input placeholder='Top-text' className='form-input' type="text"/>
                     <input placeholder='Bottom-text' className='form-input' type="text"/>
                     <button className='form-button'>Generate a new meme image</button>
-                </form>
+                </div>
             </main>
         );
     }
