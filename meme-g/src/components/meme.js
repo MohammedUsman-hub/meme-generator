ba1da979c2d16fd478ad3d94affb536ca022ba59
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-06-28 19:44:09 +0100

    practice

--- meme-g/src/components/meme.js
+++ meme-g/src/components/meme.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import "../App.css"
 
-import memesData from '../memesData';
+import memesData from '../memesData.js';
 
+function imageClick(){
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNumber].url
+}
 
-   function imageClick(){
-        const memesArray = memesData.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
-    }
 
 export default function Meme(){
-       
-    const[memeImage, setMemeImage] = React.useState("")
+    const [memeImage, setMemeImage] = React.useState("")
 
     function getMemeImage(){
         const memesArray = memesData.data.memes
@@ -20,18 +19,16 @@ export default function Meme(){
         setMemeImage(memesArray[randomNumber].url)
     }
 
-        return(
-            
-            <main>
-                <div className='form'>
-                    <input placeholder='Top-text' className='form-input' type="text"/>
-                    <input placeholder='Bottom-text' className='form-input' type="text"/>
-                    <button onClick={getMemeImage} className='form-button'>Generate a new meme image</button>
-                </div>
-                <img src={memeImage} className='meme-img'/>
-            </main>
-
-        );
-    
+    return(
+        <main>
+            <div className='form'>
+                <input placeholder='Top-Text' className='form-input' type="text"/>
+                <input placeholder='Bottom-Text' className='form-input' type='text'/>
+                <button onClick={getMemeImage} className='form-button'>Genereate a new meme Image</button>
+            </div>
+            <img src={memeImage} className='meme-img'></img>
+        </main>
+    )
 }
-/* */
+
+/**/
