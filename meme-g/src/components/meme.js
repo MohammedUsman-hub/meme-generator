f5740e8b89ae13f1113fafcdc7244ddd58efec04
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2024-03-03 17:10:50 +0000

    update mem.js

--- meme-g/src/components/meme.js
+++ meme-g/src/components/meme.js
@@ -35,6 +35,7 @@ export default function Meme(){
             <div className='form'>
                 <input placeholder='Top-Text' className='form-input' type="text"/>
                 <input placeholder='Bottom-Text' className='form-input' type='text'/>
+                
                 <button onClick={getMemeImage} className='form-button'>Genereate a new meme Image</button>
             </div>
             <img src={meme.randomImage} className='meme-img'></img>
